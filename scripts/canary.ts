c07d02d7c0f5b2d78f01ffff6e94df6bb071b710
Lukas Heizmann <49527534+lkwr@users.noreply.github.com>
2025-02-25 17:06:17 +0100

    add next release

--- scripts/canary.ts
+++ scripts/canary.ts
@@ -3,12 +3,18 @@ import dayjsUtc from "dayjs/esm/plugin/utc";
 
 dayjs.extend(dayjsUtc);
 
+const prerelease =
+	process.argv
+		.at(2)
+		?.match(/^[a-z]+$/)
+		?.at(0) ?? "canary";
+
 const packageJsonFile = Bun.file(`${import.meta.dirname}/../dist/package.json`);
 const packageJson = await packageJsonFile.json();
 
 const now = dayjs().utc().format("YYYYMMDDHHmmss");
 
-packageJson.version = `0.0.0-canary.${now}`;
+packageJson.version = `0.0.0-${prerelease}.${now}`;
 
 await packageJsonFile.write(JSON.stringify(packageJson, null, 2));
 
